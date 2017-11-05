80715e9fe25cf2c98b425cf58dc2e3a145f9ef5d
Ranjay Salmon <ranjy23@gmail.com>
2017-11-05 10:22:37 -0500

    added git ignore file

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+var VENDOR_LIBS = ['jquery'];
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+           
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract({ use: ['css-loader', 'sass-loader'] })
+            }
+            // {
+            //     test: /\.scss$/,
+            //     loader: ExtractTextPlugin.extract('css!sass')
+            // }
+           
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: './src/assets/images', to: './assets/images' }
+        ]),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'bundle']
+        })
+    ]
+}
