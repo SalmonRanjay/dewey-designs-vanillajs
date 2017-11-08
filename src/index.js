001182a13b3b1d2ee405fefe3d42f03dd28379f0
Ranjay Salmon <ranjy23@gmail.com>
2017-11-07 23:19:01 -0500

    added reveal scroll to the applicaiton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-
-
 import './assets/scss/main.scss';
+import $ from 'jquery';
+import RevealOnScroll from './scripts/modules/RevealOnScroll';
+
+
+
+new RevealOnScroll( $('.process'), "65%");
+new RevealOnScroll( $('.home_large-img'), "50%");
