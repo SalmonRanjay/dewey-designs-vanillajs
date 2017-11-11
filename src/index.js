9315fcd347726fad81a90fba8b954f4ae1ac2523
Ranjay Salmon <ranjy23@gmail.com>
2017-11-11 13:38:19 -0500

    added the work page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import RevealOnScroll from './scripts/modules/RevealOnScroll';
 import StickyHeader from './scripts/modules/StickyHeader';
 import MobileMenu from './scripts/modules/MobileMenu';
 
-
+//import './work.html';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll( $('.process'), "45%");
 new RevealOnScroll( $('.home_large-img'), "40%");
