11d55ec5a18ff4d1b6996286de89dcdc87a29d86
Ranjay Salmon <ranjy23@gmail.com>
2017-12-10 19:14:00 -0500

    updated form

--- src/scripts/modules/FormSubmit.js
+++ src/scripts/modules/FormSubmit.js
@@ -14,12 +14,12 @@ class FormSubmit {
         this.verificationText = $('#formVerification');
         this.description = $("#description");
 
-         this.firstname.val('');
-         this.company.val('');
-         this.description.val('');
-         this.email.val('');
-         this.verificationText.val('');
-         this.phone.val('');
+        this.firstname.val('');
+        this.company.val('');
+        this.description.val('');
+        this.email.val('');
+        this.verificationText.val('');
+        this.phone.val('');
 
 
         // Get the messages div.
@@ -32,6 +32,36 @@ class FormSubmit {
 
     events() {
         this.submitBTN.click(this.submitForm.bind(this));
+    }
+
+    ValidateEmail(mail) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+            return (true)
+        }
+        alert("You have entered an invalid email address!")
+        return (false)
+    }
+
+    validatePhone(phone) {
+        var error = "";
+        var stripped = phone.replace(/[\(\)\.\-\ ]/g, '');
+
+        if (stripped == "") {
+            error = "You didn't enter a phone number.";
+            alert(error);
+            return false;
+        } else if (isNaN(parseInt(stripped))) {
+            phone = "";
+            error = "The phone number contains illegal characters.";
+            alert( error);
+            return false;
+
+        } else if (!(stripped.length == 10)) {
+            phone = "";
+            error = "The phone number is the wrong length. Make sure you included an area code.\n";
+            alert(error);
+            return false;
+        }
     }
 
     submitForm(event) {
@@ -50,9 +80,20 @@ class FormSubmit {
             console.log(formData);
             // http://localhost:3000/api/mail
             // https://dewey-design-server.herokuapp.com/api/mail
+
+            if(!that.ValidateEmail(that.email.val())){
+                //alert('Invalid email');
+                return
+            }
+
+            if(!that.validatePhone(that.phone.val())){
+                
+                return ;
+            }
+
             $.ajax({
                 type: 'POST',
-                url: 'http://localhost:3000/api/mail',
+                url: 'https://dewey-design-server.herokuapp.com/api/mail',
                 data: formData
             }).done(function (response) {
                 // Make sure that the formMessages div has the 'success' class.
@@ -71,16 +112,16 @@ class FormSubmit {
                 that.phone.val('');
                 console.log("Success mail sent");
             })
-            .fail(function(data) {
-                
-                console.log("it failed");
-                that.firstname.val('');
-                that.company.val('');
-                that.email.val('');
-                that.verificationText.val('');
-                that.phone.val('');
-                that.description.val('');
-            });
+                .fail(function (data) {
+
+                    console.log("it failed");
+                    that.firstname.val('');
+                    that.company.val('');
+                    that.email.val('');
+                    that.verificationText.val('');
+                    that.phone.val('');
+                    that.description.val('');
+                });
         } else {
             // reject form
             console.log("Issue with form");
